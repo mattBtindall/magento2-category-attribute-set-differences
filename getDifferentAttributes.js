d03a93bf43ff7769ae160e0913b28d2f1f820e46
mattBtindall <matthewbtindall@gmail.com>
2023-04-04 15:56:09 +0100

    Code restyle
    Seperate functions into specific files ready to scale up when adding some post methods

---
+++ getDifferentAttributes.js
@@ -0,0 +1,69 @@
+const IMPORT_ALL_ID = 31
+const admin = require('./global')
+
+/**
+ * simple wrapper around the magento2api get method
+ * @param {String} endpoint - endpoint that gets concatenated onto url
+ * @param {Array} filters - array containing filters
+ * @returns {Promise} - containing fetched data
+ */
+function getWithFilter(endpoint, filters) {
+    return admin.get(endpoint, {
+        params: {
+            searchCriteria: {
+            'filter_groups': [
+                    {
+                        'filters': filters
+                    }
+                ]
+            }
+        }
+    })
+}
+
+/**
+ * gets all attribute codes of an attribute set
+ * @param {Number} attributeSetId - the id of the attribute set
+ * @returns {Promise} - argument contains attribute codes
+ */
+async function getAttributesFromSet(attributeSetId) {
+    const attributeCodes = []
+    const attributeSetAttributes = await admin.get(`products/attribute-sets/${attributeSetId}/attributes`)
+    attributeSetAttributes.forEach(attribute => attributeCodes.push(attribute.attribute_code))
+    return attributeCodes
+}
+
+/**
+ * gets attribute codes from products that are part of the importAll attribute set from the specified category
+ * @param {*} categoryId - magento category id
+ * @returns {Promise} - containing array of attribute codes
+ */
+async function getAttributeCodesFromProducts(categoryId) {
+    const attributeCodes = [];
+    const products = await getWithFilter('products', [
+        { 'field': 'attribute_set_id', 'value': IMPORT_ALL_ID, 'condition_type': 'eq' },
+        { 'field': 'category_id', 'value':  categoryId, 'condition_type': 'eq'}
+    ])
+
+    for (const product of products.items) {
+        const attributes = await getWithFilter(`products/${product.sku}`)
+        attributes.custom_attributes.forEach(attribute => {
+            if (!attributeCodes.includes(attribute.attribute_code)) attributeCodes.push(attribute.attribute_code);
+        })
+    }
+    return attributeCodes;
+}
+
+/**
+ * gets the attribute codes that are missing from the attribute set
+ * @param {Number} attribueSetId - attribute set id
+ * @param {Number} categoryId - category id
+ * @returns {Promise} containing an array of the differing attribute codes
+ */
+async function getDifferentAttributeCodes(attribueSetId, categoryId) {
+    const attributeSetCodes = await getAttributesFromSet(attribueSetId)
+    const categoryAttributeCodes = await getAttributeCodesFromProducts(categoryId)
+    return categoryAttributeCodes.filter(attributeCode => !attributeSetCodes.includes(attributeCode))
+}
+
+module.exports = getDifferentAttributeCodes;
---
+++ global.js
@@ -0,0 +1,20 @@
+const https = require('https')
+const keys = require('./keys')
+const Magento2Api = require('magento2-api-wrapper')
+
+const admin = new Magento2Api({
+    api: {
+        url: 'https://localhost',
+        consumerKey: keys.consumerKey,
+        consumerSecret: keys.consumerSecret,
+        accessToken: keys.accessToken,
+        tokenSecret: keys.tokenSecret
+    },
+    axios: {
+        httpsAgent: new https.Agent({
+            rejectUnauthorized: false
+        })
+    }
+})
+
+module.exports = admin
