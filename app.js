5013ef5e72066ac7499692c191eb84c4f38f789f
mattBtindall <matthewbtindall@gmail.com>
2023-04-04 14:57:44 +0100

    Get difference between categoryAttribute codes and attributeSet codes

--- app.js
+++ app.js
@@ -43,5 +43,38 @@ async function getAttributesFromSet(attributeSetId) {
     attributeSetAttributes.forEach(attribute => attributeCodes.push(attribute.attribute_code))
     return attributeCodes
 }
-// getAttributesFromSet(41)
-//     .then(data => console.log(data));
+
+/**
+ * gets attribute codes from products that are part of the importAll attribute set from the specified category
+ * @param {*} categoryId - magento category id
+ * @returns {Promise} - containing array of attribute codes
+ */
+async function getAttributeCodesFromProducts(categoryId) {
+    const attributeCodes = [];
+    const products = await getWithFilter('products', [
+        { 'field': 'attribute_set_id', 'value': IMPORT_ALL_ID, 'condition_type': 'eq' },
+        { 'field': 'category_id', 'value':  categoryId, 'condition_type': 'eq'}
+    ])
+
+    for (const product of products.items) {
+        const attributes = await getWithFilter(`products/${product.sku}`)
+        attributes.custom_attributes.forEach(attribute => {
+            if (!attributeCodes.includes(attribute.attribute_code)) attributeCodes.push(attribute.attribute_code);
+        })
+    }
+    return attributeCodes;
+}
+
+/**
+ * gets the attribute codes that are missing from the attribute set
+ * @param {Number} attribueSetId - attribute set id
+ * @param {Number} categoryId - category id
+ * @returns {Promise} containing an array of the differing attribute codes
+ */
+async function getDifferentAttributeCodes(attribueSetId, categoryId) {
+    const attributeSetCodes = await getAttributesFromSet(attribueSetId)
+    const categoryAttributeCodes = await getAttributeCodesFromProducts(categoryId)
+    return categoryAttributeCodes.filter(attributeCode => !attributeSetCodes.includes(attributeCode))
+}
+getDifferentAttributeCodes(41, 4423)
+    .then(data => console.log(data));
