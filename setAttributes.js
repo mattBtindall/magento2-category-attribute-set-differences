dc21bae21519e9f36cca588a24b06c060600e739
mattbtindall <matthewbtindall@gmail.com>
2023-04-11 14:51:42 +0100

    Add attributes to an attribute set

--- setAttributes.js
+++ setAttributes.js
@@ -1,11 +1,26 @@
 const admin = require('./global');
-const { getAttributeSetGroupId } = require('./helpers/getHelpers')
 
-admin.put('products/attribute-sets/attributes', {
-    params: {
-        "attributeSetId": 41,
-        "attributeGroupId": 0,
-        "attributeCode": "quantity",
-        "sortOrder": 0
+/**
+ *
+ * @param {Number} attributeSetId - attribute set id
+ * @param {Number} attributeGroupId - attribute group id
+ * @param {Array.<String>} attributeCodes - Array of attribute codes
+ * @param {Number} sortOrder - sort order
+ * @returns {Array.<Number>} the attribute ids that have been added to the set
+ */
+async function addAttributesToSet(attributeSetId, attributeGroupId, attributeCodes, sortOrder = 0) {
+    const attributeIds = []
+    for (const attributeCode of attributeCodes) {
+        attributeIds.push(await admin.post('products/attribute-sets/attributes', {
+            attributeSetId,
+            attributeGroupId,
+            attributeCode,
+            sortOrder
+        }))
     }
-});
+    return attributeIds
+}
+
+module.exports = {
+    addAttributesToSet
+}
