12e433d50032812b119e19036a8a1b29a0eb3010
mattbtindall <matthewbtindall@gmail.com>
2023-04-12 10:51:06 +0100

    Add missing attributes to attribute set

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const getDifferentAttributeCodes = require('./getDifferentAttributes')
+const { getAttributeSetGroupId, getAttributeSetId, getCategoryId } = require('./helpers/getHelpers')
+const { addAttributesToSet } = require('./setAttributes')
 
-getDifferentAttributeCodes(41, 4423)
-    .then(data => console.log(data));
+async function addAttributes(attributeSet,  attributeGroup, category, sortOrder = 0) {
+    const attributeSetId = typeof attributeSet === 'string' ? await getAttributeSetId(attributeSet) : attributeSet
+    const attributeGroupId = typeof attributeGroup === 'string' ? await getAttributeSetGroupId(attributeSetId, attributeGroup) : attributeGroup
+    const categoryId = typeof category === 'string' ? await getCategoryId(category) : category
+
+    const attributeCodes = await getDifferentAttributeCodes(attributeSetId, categoryId)
+    return addAttributesToSet(attributeSetId, attributeGroupId, attributeCodes)
+}
+addAttributes('Bins Specifications', 'General', 'Wheelie Bins ')
+    .then(data => console.log(data))
