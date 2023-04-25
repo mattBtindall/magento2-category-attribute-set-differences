853050257cbeb4cf53c57688af17c1e3afaa4498
mattBtindall <matthewbtindall@gmail.com>
2023-04-25 09:47:12 +0100

    Pull all methods togethere to perform expected task
    Update app so that attributes are added to an existing set and then the importAll products (specified through category) are added to that attributes set

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const getDifferentAttributeCodes = require('./getDifferentAttributes')
-const { getAttributeSetGroupId, getAttributeSetId, getCategoryId } = require('./helpers/getHelpers')
-const { addAttributesToSet } = require('./setAttributes')
+const { getAttributeSetGroupId, getAttributeSetId, getCategoryId, getCategoryImportAllProducts, getDifferentAttributeCodes } = require('./helpers/getHelpers')
+const { addAttributesToSet, updateProductAttributeSet } = require('./helpers/setHelpers')
+const { attributeSetIds } = require('./global')
 
-async function addAttributes(attributeSet,  attributeGroup, category, sortOrder = 0) {
+async function addAttributes(attributeSet,  attributeGroup, category, sortOrder = 500) {
     const attributeSetId = typeof attributeSet === 'string' ? await getAttributeSetId(attributeSet) : attributeSet
     const attributeGroupId = typeof attributeGroup === 'string' ? await getAttributeSetGroupId(attributeSetId, attributeGroup) : attributeGroup
     const categoryId = typeof category === 'string' ? await getCategoryId(category) : category
 
-    const attributeCodes = await getDifferentAttributeCodes(attributeSetId, categoryId)
-    return addAttributesToSet(attributeSetId, attributeGroupId, attributeCodes)
+    let attributeCodes = await getDifferentAttributeCodes(attributeSetId, categoryId, attributeSetIds.importAll)
+    console.log(attributeCodes)
+    console.log(`Codes length: ${attributeCodes.length}`)
+    await addAttributesToSet(attributeSetId, attributeGroupId, attributeCodes, sortOrder)
+
+    const importAllProducts = await getCategoryImportAllProducts(categoryId, attributeSetIds.importAll)
+    console.log(importAllProducts.length)
+    for (const product of importAllProducts) {
+        await updateProductAttributeSet(product.sku, attributeSetId)
+    }
 }
-addAttributes('Bins Specifications', 'General', 'Wheelie Bins ')
-    .then(data => console.log(data))
+addAttributes('Bins Specifications', 'Attributes', 'Wheelie Bins ') // 100 products
+// addAttributes('Benches and Picnic Tables', 'Attributes', 'Seating') // also need to change name of set to 'Seating, Benches and Picnic Tables'
+// addAttributes('Tank Specification', 'Attributes', 'Cold Water Tanks')
+// addAttributes('Tank Specification', 'Attributes', 'Open Top Water Tanks')
+// addAttributes('Tank Specification', 'Attributes', 'Water Tanks')
+// addAttributes('Bins Specifications', 'Attributes', 'Litter & Waste Bins') // 5413
+// addAttributes('Bins Specifications', 'Attributes', 'Recycling Bins ') // 7246
+// addAttributes('Bins Specifications', 'Attributes', 'Clinical Waste Bins') // 204
+    // .then(data => console.log(data))
