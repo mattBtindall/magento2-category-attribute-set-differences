192763964c18eb06e993892a82bd6017390d87ab
mattBtindall <matthewbtindall@gmail.com>
2023-04-26 11:52:48 +0100

    Function to compare two products
    Accepts an array of attribute codes ignore
    Only compares them on the top level, so if there is any differences within objects these will not be detailed

---
+++ testing.js
@@ -0,0 +1,43 @@
+const { admin, ATTRIBUTES_TO_REMOVE, generateRandomNum } = require('./global')
+const Magento2Api = require('magento2-api-wrapper')
+const keys = require('./keys')
+const https = require('https')
+
+const liveAdmin = new Magento2Api({
+    api: {
+        url: 'https://magento-admin.kingfisherdirect.co.uk',
+        consumerKey: keys.live.consumerKey,
+        consumerSecret: keys.live.consumerSecret,
+        accessToken: keys.live.accessToken,
+        tokenSecret: keys.live.tokenSecret
+    },
+    axios: {
+        httpsAgent: new https.Agent({
+            rejectUnauthorized: false
+        })
+    }
+})
+
+/**
+ * compares two products and returns the key (attribute_code) if the values are different
+ * @param {Object} liveProduct - magento product
+ * @param {Object} localProduct- magento product
+ * @param {Array.<string>} attributesToIgnore - attribute codes that aren't tested
+ * @returns {Array} containing attributes that are different
+ */
+function compareAttributes(liveProduct, localProduct, attributesToIgnore) {
+    const differences = []
+    for (const key in localProduct) {
+        if (attributesToIgnore.includes(key)) continue
+        let localAttribute = localProduct[key]
+        let liveAttribute = liveProduct[key]
+
+        if (typeof localProduct[key] === 'object') {
+            localAttribute = JSON.stringify(localProduct[key])
+            liveAttribute = JSON.stringify(liveProduct[key])
+        }
+
+        if (localAttribute !== liveAttribute) differences.push(key)
+    }
+    return differences
+}
